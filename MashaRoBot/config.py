2477e871a2f4fc630677cd21821e85707a2364a7
Nagarajking1 <97332888+Nagarajking1@users.noreply.github.com>
2022-02-01 11:59:13 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -41,16 +41,16 @@ class Config(object):
     WOLVES = get_user_list('elevated_users.json', 'whitelists')
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 10
+    PORT = 5000
     DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     WORKERS = 100  # Number of subthreads to use. Set as number of threads your processor uses
-    BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = 'CAACAgUAAxkBAAEDyBVh-NMeDTdzKypJrfCYKTJAGOyLuwACTAUAAu-r0VW1EtKPPK-RqiME'  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
-    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
+    CASH_API_KEY = 'AS5LYK9Z7227QYCJ'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = 'QM8PH28330UD'  # Get your API key from https://timezonedb.com/api
     WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
-    AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = '1bf1e6aecf90a6861d4d5876daefdb87c718a73f4b3168dce7cc86823ae89537e1212ebfb2bbd859ddb1cc7fce78f113130289273859cac0f4d4763f67de3fc6'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
